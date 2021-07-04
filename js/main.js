067bb2c59499a39fea824e5eb86bd815248c32b9
榊原陽 <sakakibara@sakakibara.local>
2021-07-04 19:41:17 +0900

    Finish 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-addEventListener
+$(function(){
+  　$(window).scroll(function (){
+      $('.effect-fade').each(function(){
+          var elemPos = $(this).offset().top;
+          var scroll = $(window).scrollTop();
+          var windowHeight = $(window).height();
+          if (scroll > elemPos - windowHeight){
+              $(this).addClass('effect-scroll');
+          }
+      });
+  　});
+  });
