40d5ecf3134ad3c1104a15026f99e1071007fab1
榊原陽 <sakakibara@sakakibara.local>
2021-07-11 17:56:38 +0900

    Modified styles.css

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
-$(function(){
-  　$(window).scroll(function (){
-      $('.effect-fade').each(function(){
-          var elemPos = $(this).offset().top;
-          var scroll = $(window).scrollTop();
-          var windowHeight = $(window).height();
-          if (scroll > elemPos - windowHeight){
-              $(this).addClass('effect-scroll');
-          }
-      });
-  　});
-  });
+$(window).scroll(function (){
+    $(".marker-animation").each(function(){
+      var position = $(this).offset().top; //ページの一番上から要素までの距離を取得
+      var scroll = $(window).scrollTop(); //スクロールの位置を取得
+      var windowHeight = $(window).height(); //ウインドウの高さを取得
+      if (scroll > position - windowHeight){ //スクロール位置が要素の位置を過ぎたとき
+        $(this).addClass('active'); //クラス「active」を与える
+      }
+    });
+   });
