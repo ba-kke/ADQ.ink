c526c5518a35c7b3d159cb69a0ee0f16f767b130
ba-kke <obakebake1223@gmail.com>
2021-07-29 19:16:05 +0900

    img change & modified text

--- js/main.js
+++ js/main.js
@@ -36,16 +36,3 @@ $(window).scroll(function (){
       }
     });
    });
-
-$(function(){
-  var scrollend = $('#footer').offset().top ; //ページ上部からの距離を取得
-  var distance = 0;
-  $(document).scroll(function(){
-    distance = $(this).scrollTop(); //スクロールした距離を取得
-    if (scrollend <= distance) {
-      $('.top-view').fadeOut();
-    } else{
-      $('.top-view').fadeIn();
-    }
-  });
-});
